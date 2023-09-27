2ae6e2bd2a9cc28037579a30b32e7d11931a67f7
Marten Gartner <martenwallewein@web.de>
2023-09-27 10:25:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SCIERA'
+copyright = '2023, Ovgu / ETH'
+author = 'Ovgu Magdeburg, ETH Zurich'
 
 release = '0.1'
 version = '0.1.0'
